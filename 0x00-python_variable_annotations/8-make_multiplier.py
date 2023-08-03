e30a7ea0acd983461aac6a4acc81c5b97ee45f72
Martins100-Tmd <Olaitanfabayo@gmail.com>
2023-08-03 12:37:27 +0000

    Backend Python - Worked on type annotations

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
-
+'''
+Task 9 module
+'''
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''
+    return a float callable (function)
+    '''
     return lambda x: x * multiplier
