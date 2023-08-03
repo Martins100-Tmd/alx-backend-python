de36a2186593e046f31f1aa1fbf4f1d162cd3f8c
Martins100-Tmd <Olaitanfabayo@gmail.com>
2023-08-03 12:33:18 +0000

    Backend Python - Worked on type annotations

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -9,10 +9,4 @@ def sum_mixed_list(mxd_list: List[Union[float, int]]) -> float:
     '''
     adds up list of float and int
     '''
-    sum = 0.0
-    count = 0
-
-    while count < len(mxd_list):
-        sum += float(mxd_list[count])
-        count += 1
-    return sum
+    return float(sum(mxd_list))
