b3b9734d13ce78924cc1e4c539cb1b2536a79289
Martins100-Tmd <Olaitanfabayo@gmail.com>
2023-08-03 12:31:02 +0000

    Backend Python - Worked on type annotations

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
-
+'''
+Task 8 module
+'''
 from typing import Union, Tuple
 from math import pow
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''
+    return tuple of str and float
+    '''
     if type(v).__name__ == 'int':
         return (k, int(pow(v, 2)))
     return (k, float(pow(v, 2)))
