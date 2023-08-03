76b140dc7f242bd8fb955eed09456a26093d2129
Martins100-Tmd <Olaitanfabayo@gmail.com>
2023-08-03 13:05:50 +0000

    Backend Python - Worked on type annotations

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''
+Task 13 module
+'''
+from typing import Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    '''
+    Make a list outta tuple
+    '''
+    zoomed_in: Tuple = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = tuple([12, 72, 91])
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
+
+print(zoom_2x)
+print(zoom_3x)
