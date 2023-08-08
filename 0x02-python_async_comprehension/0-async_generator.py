d0f090f60fb4f35f1c756e9d332f779ccee27f4a
Martins100-Tmd <Olaitanfabayo@gmail.com>
2023-08-08 20:08:05 +0000

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Task 0's module
+"""
+import asyncio
+from typing import AsyncGenerator, Any
+import random
+
+
+async def async_generator() -> AsyncGenerator[float, Any]:
+    """
+    yields a random number range 0 - 10
+    """
+    for i in range(10):
+        rand = random.uniform(0, 10)
+        await asyncio.sleep(1)
+        yield rand
